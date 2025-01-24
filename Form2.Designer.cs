fc17a40fb77865c296b1d786cedf25034869be16
2003-Anonymous <J.Sommerhalder.inf24@stud.bbbaden.ch>
2025-01-24 11:44:46 +0100

    Add files via upload

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -28,138 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            button6 = new Button();
-            button7 = new Button();
-            button8 = new Button();
-            button9 = new Button();
             btnRestart = new Button();
             btnBack = new Button();
             label1 = new Label();
             btn4x4 = new Button();
             btn5x5 = new Button();
+            btn3x3 = new Button();
             SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(233, 102);
-            button1.Name = "button1";
-            button1.Size = new Size(70, 70);
-            button1.TabIndex = 0;
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += CheckButton;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(323, 102);
-            button2.Name = "button2";
-            button2.Size = new Size(70, 70);
-            button2.TabIndex = 1;
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += CheckButton;
-            // 
-            // button3
-            // 
-            button3.Location = new Point(414, 102);
-            button3.Name = "button3";
-            button3.Size = new Size(70, 70);
-            button3.TabIndex = 2;
-            button3.UseVisualStyleBackColor = true;
-            button3.Click += CheckButton;
-            // 
-            // button4
-            // 
-            button4.Location = new Point(233, 189);
-            button4.Name = "button4";
-            button4.Size = new Size(70, 70);
-            button4.TabIndex = 3;
-            button4.UseVisualStyleBackColor = true;
-            button4.Click += CheckButton;
-            // 
-            // button5
-            // 
-            button5.Location = new Point(323, 189);
-            button5.Name = "button5";
-            button5.Size = new Size(70, 70);
-            button5.TabIndex = 4;
-            button5.UseVisualStyleBackColor = true;
-            button5.Click += CheckButton;
-            // 
-            // button6
-            // 
-            button6.Location = new Point(414, 189);
-            button6.Name = "button6";
-            button6.Size = new Size(70, 70);
-            button6.TabIndex = 5;
-            button6.UseVisualStyleBackColor = true;
-            button6.Click += CheckButton;
-            // 
-            // button7
-            // 
-            button7.Location = new Point(233, 281);
-            button7.Name = "button7";
-            button7.Size = new Size(70, 70);
-            button7.TabIndex = 6;
-            button7.UseVisualStyleBackColor = true;
-            button7.Click += CheckButton;
-            // 
-            // button8
-            // 
-            button8.Location = new Point(323, 281);
-            button8.Name = "button8";
-            button8.Size = new Size(70, 70);
-            button8.TabIndex = 7;
-            button8.UseVisualStyleBackColor = true;
-            button8.Click += CheckButton;
-            // 
-            // button9
-            // 
-            button9.Location = new Point(414, 281);
-            button9.Name = "button9";
-            button9.Size = new Size(70, 70);
-            button9.TabIndex = 8;
-            button9.UseVisualStyleBackColor = true;
-            button9.Click += CheckButton;
             // 
             // btnRestart
             // 
-            btnRestart.Location = new Point(590, 175);
+            btnRestart.Location = new Point(23, 219);
             btnRestart.Name = "btnRestart";
             btnRestart.Size = new Size(112, 34);
             btnRestart.TabIndex = 9;
             btnRestart.Text = "Restart";
             btnRestart.UseVisualStyleBackColor = true;
+            btnRestart.Visible = false;
             btnRestart.Click += btnRestart_Click;
             // 
             // btnBack
             // 
-            btnBack.Location = new Point(590, 215);
+            btnBack.Location = new Point(23, 259);
             btnBack.Name = "btnBack";
             btnBack.Size = new Size(112, 34);
             btnBack.TabIndex = 10;
             btnBack.Text = "Back";
             btnBack.UseVisualStyleBackColor = true;
+            btnBack.Visible = false;
             btnBack.Click += btnBack_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("SimSun-ExtG", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(291, 41);
+            label1.Location = new Point(12, 44);
             label1.Name = "label1";
             label1.Size = new Size(147, 28);
             label1.TabIndex = 11;
             label1.Text = "Player: X";
+            label1.Visible = false;
             // 
             // btn4x4
             // 
-            btn4x4.Location = new Point(59, 141);
+            btn4x4.Location = new Point(297, 194);
             btn4x4.Name = "btn4x4";
-            btn4x4.Size = new Size(112, 34);
+            btn4x4.Size = new Size(166, 59);
             btn4x4.TabIndex = 12;
             btn4x4.Text = "4 X 4";
             btn4x4.UseVisualStyleBackColor = true;
@@ -167,33 +81,35 @@
             // 
             // btn5x5
             // 
-            btn5x5.Location = new Point(59, 207);
+            btn5x5.Location = new Point(297, 289);
             btn5x5.Name = "btn5x5";
-            btn5x5.Size = new Size(112, 34);
+            btn5x5.Size = new Size(166, 59);
             btn5x5.TabIndex = 13;
             btn5x5.Text = "5 X 5";
             btn5x5.UseVisualStyleBackColor = true;
             btn5x5.Click += btn5x5_Click;
+            // 
+            // btn3x3
+            // 
+            btn3x3.Location = new Point(297, 101);
+            btn3x3.Name = "btn3x3";
+            btn3x3.Size = new Size(166, 59);
+            btn3x3.TabIndex = 14;
+            btn3x3.Text = "3 X 3";
+            btn3x3.UseVisualStyleBackColor = true;
+            btn3x3.Click += btn3x3_Click;
             // 
             // Form2
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btn3x3);
             Controls.Add(btn5x5);
             Controls.Add(btn4x4);
             Controls.Add(label1);
             Controls.Add(btnBack);
             Controls.Add(btnRestart);
-            Controls.Add(button9);
-            Controls.Add(button8);
-            Controls.Add(button7);
-            Controls.Add(button6);
-            Controls.Add(button5);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
             Name = "Form2";
             Text = "TicTacToe";
             ResumeLayout(false);
@@ -201,20 +117,11 @@
         }
 
         #endregion
-
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
-        private Button button5;
-        private Button button6;
-        private Button button7;
-        private Button button8;
-        private Button button9;
         private Button btnRestart;
         private Button btnBack;
         private Label label1;
         private Button btn4x4;
         private Button btn5x5;
+        private Button btn3x3;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -18,7 +18,8 @@ namespace WinForms1
         }
 
         int turnCounter = 0;
-
+        int numberButtons;
+        int gridSize = 3;
 
         private void CheckButton(object sender, EventArgs e)
         {
@@ -33,7 +34,7 @@ namespace WinForms1
                     MessageBox.Show($"Spieler {myButton.Text} hat gewonnen!");
                     btnRestart_Click(null, null);
                 }
-                else if (turnCounter == 9)
+                else if (turnCounter == numberButtons)
                 {
                     MessageBox.Show("Unentschieden!");
                     btnRestart_Click(null, null);
@@ -45,17 +46,17 @@ namespace WinForms1
         private void btnRestart_Click(object sender, EventArgs e)
         {
             turnCounter = 0;
-            button1.Text = "";
-            button2.Text = "";
-            button3.Text = "";
-            button4.Text = "";
-            button5.Text = "";
-            button6.Text = "";
-            button7.Text = "";
-            button8.Text = "";
-            button9.Text = "";
+
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is Button btn && btn != btnRestart && btn != btnBack && btn != btn4x4 && btn != btn5x5 && btn != btn3x3)
+                {
+                    btn.Text = "";
+                }
+            }
 
             label1.Text = "Player: X";
+
         }
         private string XorO()
         {
@@ -63,24 +64,7 @@ namespace WinForms1
         }
 
 
-        private bool CheckWin()
-        {
-            // Horizontale Reihen
-            if ((button1.Text == button2.Text && button2.Text == button3.Text && !string.IsNullOrEmpty(button1.Text)) ||
-                (button4.Text == button5.Text && button5.Text == button6.Text && !string.IsNullOrEmpty(button4.Text)) ||
-                (button7.Text == button8.Text && button8.Text == button9.Text && !string.IsNullOrEmpty(button7.Text)) ||
-                // Vertikale Reihen
-                (button1.Text == button4.Text && button4.Text == button7.Text && !string.IsNullOrEmpty(button1.Text)) ||
-                (button2.Text == button5.Text && button5.Text == button8.Text && !string.IsNullOrEmpty(button2.Text)) ||
-                (button3.Text == button6.Text && button6.Text == button9.Text && !string.IsNullOrEmpty(button3.Text)) ||
-                // Diagonalen
-                (button1.Text == button5.Text && button5.Text == button9.Text && !string.IsNullOrEmpty(button1.Text)) ||
-                (button3.Text == button5.Text && button5.Text == button7.Text && !string.IsNullOrEmpty(button3.Text)))
-            {
-                return true;
-            }
-            return false;
-        }
+
 
         private void btnBack_Click(object sender, EventArgs e)
         {
@@ -94,13 +78,216 @@ namespace WinForms1
 
         private void btn4x4_Click(object sender, EventArgs e)
         {
-            Button btn4 = new Button();
-            btn4.Location = new Point(40);
+            MoveButtons();
+            DeleteButtons();
+            btnRestart_Click(sender, e);
+            btn3x3_Click(sender, e);
+            numberButtons = 16;
+            gridSize = 4;
+            List<Point> positions = new List<Point>
+            {
+                new Point(505, 102),
+                new Point(505, 189),
+                new Point(505, 281),
+                new Point(505, 363),
+                new Point(414, 363),
+                new Point(323, 363),
+                new Point(233, 363)
+            };
+
+            for (int i = 0; i < positions.Count; i++)
+            {
+                Button btn = new Button();
+                btn.Location = positions[i];
+                btn.Size = new Size(70, 70);
+                btn.Click += CheckButton;
+                btn.Tag = "Field";
+                this.Controls.Add(btn);
+            }
+
+            
         }
 
         private void btn5x5_Click(object sender, EventArgs e)
         {
+            MoveButtons();
+            DeleteButtons();
+            btnRestart_Click(sender, e);            
+            btn3x3_Click(sender, e);
+            btn4x4_Click(sender, e);            
+            numberButtons = 25;
+            gridSize = 5;
+            List<Point> positions2 = new List<Point>
+            {
+                new Point(596,102),
+                new Point(596,189),
+                new Point(596,281),
+                new Point(596,363),               
+                new Point(233,15),
+                new Point(323,15),
+                new Point(414,15),
+                new Point(505,15),
+                new Point(596,15)
+            };
 
+            for (int i = 0; i < positions2.Count; i++)
+            {
+                Button btn = new Button();
+                btn.Location = positions2[i];
+                btn.Size = new Size(70, 70);
+                btn.Click += CheckButton;
+                btn.Tag = "Field";
+                this.Controls.Add(btn);
+            }
+            
         }
+
+        private void btn3x3_Click(object sender, EventArgs e)
+        {
+            MoveButtons();
+            DeleteButtons();
+            numberButtons = 9;
+            List<Point> positions3 = new List<Point>
+            {
+                new Point(233,102),
+                new Point(323,102),
+                new Point(414,102),
+                new Point(233,189),
+                new Point(323,189),
+                new Point(414,189),
+                new Point(233,281),
+                new Point(323,281),
+                new Point(414,281)
+
+            };
+            for (int i = 0; i < positions3.Count; i++)
+            {
+                Button btn = new Button();
+                btn.Location = positions3[i];
+                btn.Size = new Size(70, 70);
+                btn.Click += CheckButton;
+                btn.Tag = "Field";
+                this.Controls.Add(btn);
+            }
+            
+        }
+
+        private void MoveButtons()
+        {
+            btn5x5.Location = new Point(23, 179);
+            btn5x5.Size = new Size(112, 34);
+
+            btn4x4.Location = new Point(23, 139);
+            btn4x4.Size = new Size(112, 34);
+
+            btn3x3.Location = new Point(23, 99);
+            btn3x3.Size = new Size(112, 34);
+
+            label1.Visible = true;
+
+            btnRestart.Visible = true;
+
+            btnBack.Visible = true;           
+        }
+
+        private void DeleteButtons()
+        {
+            List<Button> buttonsToDelete = new List<Button>();
+
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is Button btn && btn.Tag?.ToString() == "Field")
+                {
+                    buttonsToDelete.Add(btn);
+                }
+            }
+
+            foreach (Button btn in buttonsToDelete)
+            {
+                this.Controls.Remove(btn);
+                btn.Dispose();
+            }
+        }
+
+        private bool CheckWin()
+        {
+            // Alle Buttons mit Tag "Field" sammeln
+            List<Button> buttons = new List<Button>();
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is Button btn && btn.Tag?.ToString() == "Field")
+                {
+                    buttons.Add(btn);
+                }
+            }
+
+            // Sicherstellen, dass wir genau die richtige Anzahl an Buttons haben
+            int expectedButtons = gridSize * gridSize;
+            if (buttons.Count != expectedButtons)
+            {
+                MessageBox.Show($"Fehler: Erwartet wurden {expectedButtons} Buttons, gefunden: {buttons.Count}");
+                return false;
+            }
+
+            // Buttons nach Reihenfolge im Grid sortieren
+            buttons = buttons.OrderBy(b => b.Location.Y).ThenBy(b => b.Location.X).ToList();
+
+            // Spielfeld (2D-Array) befüllen
+            Button[,] grid = new Button[gridSize, gridSize];
+            for (int i = 0; i < buttons.Count; i++)
+            {
+                int row = i / gridSize;
+                int col = i % gridSize;
+                grid[row, col] = buttons[i];
+            }
+
+            // Gewinnprüfung: horizontale, vertikale und diagonale Linien prüfen
+            for (int i = 0; i < gridSize; i++)
+            {
+                // Horizontale Prüfung
+                if (IsWinningLine(grid[i, 0]?.Text, grid, i, 0, 0, 1))
+                    return true;
+
+                // Vertikale Prüfung
+                if (IsWinningLine(grid[0, i]?.Text, grid, 0, i, 1, 0))
+                    return true;
+            }
+
+            // Diagonale von oben links nach unten rechts prüfen
+            if (IsWinningLine(grid[0, 0]?.Text, grid, 0, 0, 1, 1))
+                return true;
+
+            // Diagonale von oben rechts nach unten links prüfen
+            if (IsWinningLine(grid[0, gridSize - 1]?.Text, grid, 0, gridSize - 1, 1, -1))
+                return true;
+
+            return false; // Kein Gewinner gefunden
+        }
+        private bool IsWinningLine(string startText, Button[,] grid, int row, int col, int rowStep, int colStep)
+        {
+            if (string.IsNullOrEmpty(startText))
+                return false; // Leeres Feld bedeutet keine Gewinnlinie
+
+            for (int i = 0; i < gridSize; i++)
+            {
+                // Schutz vor IndexOutOfRange: Prüfen, ob die Position gültig ist
+                if (row < 0 || row >= gridSize || col < 0 || col >= gridSize)
+                    return false;
+
+                if (grid[row, col]?.Text != startText)
+                    return false; // Unterschiedlicher Text -> Keine Gewinnlinie
+
+                row += rowStep; // Nächste Position
+                col += colStep;
+            }
+
+            return true; // Alle Felder in der Linie haben denselben Text
+        }
+
+
+
+
+
+
     }
 }
